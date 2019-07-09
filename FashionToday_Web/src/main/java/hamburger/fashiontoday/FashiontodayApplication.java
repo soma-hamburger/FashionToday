5de977501feb2132e9cbef2d19701f62a1d8e2a0
SimKiSeong <totokisung@naver.com>
2019-07-09 15:38:14 +0900

    feat : h2 Console

--- FashionToday_Web/src/main/java/hamburger/fashiontoday/FashiontodayApplication.java
+++ FashionToday_Web/src/main/java/hamburger/fashiontoday/FashiontodayApplication.java
@@ -1,13 +1,13 @@
-package com.hamburger.demo;
+package hamburger.fashiontoday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FashiontodayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(FashiontodayApplication.class, args);
     }
 
 }
--- FashionToday_Web/src/test/java/hamburger/fashiontoday/FashiontodayApplicationTests.java
+++ FashionToday_Web/src/test/java/hamburger/fashiontoday/FashiontodayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hamburger.demo;
+package hamburger.fashiontoday;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class FashiontodayApplicationTests {
 
     @Test
     public void contextLoads() {
