f27fc95a840218b3cf4dc0324540e36a8ef2d396
SimKiSeong <totokisung@naver.com>
2019-08-27 16:43:54 +0900

    feat : login

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/controller/LoginController.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/controller/LoginController.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
 // 로그인 뷰를 내려주는 컨트롤러 입니다.
 @Controller
 public class LoginController {
 
-    //로그인 토큰을 받아주는 카카오API서비스
+   //로그인 토큰을 받아주는 카카오API서비스
     @Autowired
     private KakaoAPI kakaoAPI;
 
@@ -24,9 +27,15 @@ public class LoginController {
     // 로그인 요청을 담당하는 메소드
     // 로그인 이후 사용자 코드를 받아 토큰을 반환함
     @RequestMapping(value="/login")
-    public String login(@RequestParam("code") String code) {
+    public String login(@RequestParam("code") String code, HttpSession session) {
         String access_Token = kakaoAPI.getAccessToken(code);
         System.out.println("controller access_token : " + access_Token);
+        HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_Token);
+
+        if (userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_Token", access_Token);
+        }
 
         return "index";
     }
--- FashionToday_Back/src/main/java/hamburger/fashiontoday/service/KakaoAPI.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/service/KakaoAPI.java
@@ -7,7 +7,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
+import com.google.gson.JsonObject;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonElement;
@@ -72,4 +74,51 @@ public class KakaoAPI {
 
         return access_Token;
     }
+
+    public HashMap<String, Object> getUserInfo (String access_Token) {
+
+        //    요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+        HashMap<String, Object> userInfo = new HashMap<>();
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            //    요청에 필요한 Header에 포함될 내용
+            conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+            String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+            String email = kakao_account.getAsJsonObject().get("email").getAsString();
+
+            userInfo.put("nickname", nickname);
+            userInfo.put("email", email);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return userInfo;
+    }
+
 }
