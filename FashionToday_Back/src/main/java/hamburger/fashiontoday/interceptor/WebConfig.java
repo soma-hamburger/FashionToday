d6f3f59e85d4b00cb20676130670d124b1b00a48
SimKiSeong <totokisung@naver.com>
2019-10-08 20:47:58 +0900

    feat : allowOrigins

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/WebConfig.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/WebConfig.java
@@ -2,10 +2,7 @@ package hamburger.fashiontoday.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,6 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(jwtInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns(EXCLUDE_PATHS);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 
 }
