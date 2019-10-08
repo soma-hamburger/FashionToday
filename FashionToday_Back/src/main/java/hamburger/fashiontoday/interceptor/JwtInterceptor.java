41a0ae3e932a52d61ae16c51aaf4fcb1c6f0cdac
SimKiSeong <totokisung@naver.com>
2019-10-08 23:07:53 +0900

    response header

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/JwtInterceptor.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/JwtInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
@@ -24,7 +25,14 @@ public class JwtInterceptor implements HandlerInterceptor {
 
         System.out.println();
         System.out.println("동훈이 토큰 : "+token);
-        System.out.println(request.toString());
+        System.out.println("<html><body>");
+        Enumeration headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()) {
+            String name = (String)headerNames.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + " : " + value + "<br>");
+        }
+        System.out.println("</body></html>");
         System.out.println();
 
         if(token != null && jwtService.isUsable(token)){
