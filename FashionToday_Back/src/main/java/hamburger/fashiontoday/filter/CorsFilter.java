e278b1a6f8cd4a0fd72badb15114fb435b76b515
SimKiSeong <totokisung@naver.com>
2019-10-11 18:56:05 +0900

    feat : exposed header

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/filter/CorsFilter.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/filter/CorsFilter.java
@@ -15,8 +15,8 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Request-Headers", "*");
         response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control_Expose-Headers","*");
         chain.doFilter(req, res);
     }
 
