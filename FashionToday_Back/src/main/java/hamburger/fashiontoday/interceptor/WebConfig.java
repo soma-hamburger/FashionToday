d460116c27ad1bd6ff635cb11f76b166770e0085
SimKiSeong <totokisung@naver.com>
2019-10-11 17:26:23 +0900

    remove

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/WebConfig.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/WebConfig.java
@@ -17,8 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
                 .maxAge(3600);
     }
 
