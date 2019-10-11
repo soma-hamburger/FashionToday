09aa7e7552a323311e52ce9f2f9a66059caa08f2
SimKiSeong <totokisung@naver.com>
2019-10-11 17:48:58 +0900

    feat : filter

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/WebConfig.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/WebConfig.java
@@ -1,6 +1,8 @@
 package hamburger.fashiontoday.interceptor;
 
+import hamburger.fashiontoday.filter.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
@@ -13,19 +15,24 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private JwtInterceptor jwtInterceptor;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-        .allowedMethods("*")
-        .allowedHeaders("*");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//        .allowedMethods("*")
+//        .allowedHeaders("*");
+//    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns(EXCLUDE_PATHS);
+    }
+
+    @Bean
+    CorsFilter corsFilter(){
+        return new CorsFilter();
     }
 
 }
