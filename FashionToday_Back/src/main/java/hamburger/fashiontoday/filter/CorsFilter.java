00acc8f25b54d0f6bc20723feaab94063b975b13
SimKiSeong <totokisung@naver.com>
2019-10-11 17:52:46 +0900

    feat : allow all

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/filter/CorsFilter.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/filter/CorsFilter.java
@@ -15,7 +15,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(req, res);
     }
 
