acd0b54e805605adfc04ba7d915b35868e60903a
SimKiSeong <totokisung@naver.com>
2019-10-11 16:37:48 +0900

    fix

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/WebConfig.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/interceptor/WebConfig.java
@@ -26,6 +26,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3600);
     }
