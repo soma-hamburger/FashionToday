6a5c327f777347f75628e99c58d4970180bdd14f
dhoonjang <dhoonjang@gmail.com>
2019-11-20 19:38:20 +0900

    fix: remove console log #2

--- fashion-today_front/src/components/Common/ApiTest.jsx
+++ fashion-today_front/src/components/Common/ApiTest.jsx
@@ -4,7 +4,6 @@ import { UserPost } from '../../Tool';
 import { UserContext } from '../../Context';
 
 const get200 = async token => {
-  console.log(token);
   const res = await UserPost('userInfo', token, { body: 'body' });
   console.log(res);
 };
