fc2d8b847898ccee2c459ec999647319f1eec4e7
SimKiSeong <totokisung@naver.com>
2019-11-21 16:22:55 +0900

    fix : url

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/controller/RecommendController.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/controller/RecommendController.java
@@ -88,7 +88,7 @@ public class RecommendController {
 
         // 업로드
         try {
-            imgUrl = s3Uploader.upload(multipartFile, String.valueOf(loginMemberId));
+            imgUrl = s3Uploader.upload(multipartFile, String.valueOf(requestorId));
         } catch (Exception e) {
             tmpLookInfo.setRemark("upload error");
             return tmpLookInfo;
