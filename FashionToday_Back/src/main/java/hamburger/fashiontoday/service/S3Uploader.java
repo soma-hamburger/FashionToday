8651b5ceb647ce07a761e7a7fb797e6c9db27761
SimKiSeong <totokisung@naver.com>
2019-11-22 01:46:37 +0900

    fix : https url

--- FashionToday_Back/src/main/java/hamburger/fashiontoday/service/S3Uploader.java
+++ FashionToday_Back/src/main/java/hamburger/fashiontoday/service/S3Uploader.java
@@ -110,7 +110,7 @@ public class S3Uploader {
             }
         }
 
-        fixImageUrl = "https:/data.pashiontoday.com/"+uploadImageUrl.substring(47);
+        fixImageUrl = "https://data.pashiontoday.com/"+uploadImageUrl.substring(47);
         return fixImageUrl;
 
     }
